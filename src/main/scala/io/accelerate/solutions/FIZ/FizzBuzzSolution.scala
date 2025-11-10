d80f1022ce33fa016bc88334de6da11bbe16d56b
sbx_user1051 <sbx_user1051@169.254.44.229>
2025-11-10 21:07:00 +0000

    Mon Nov 10 21:07:00 UTC 2025

--- src/main/scala/io/accelerate/solutions/FIZ/FizzBuzzSolution.scala
+++ src/main/scala/io/accelerate/solutions/FIZ/FizzBuzzSolution.scala
@@ -4,7 +4,10 @@ class FizzBuzzSolution {
   def fizzBuzz(number: Int): String =
     (number % 3 == 0, number % 5 == 0) match {
       case (true, true) => "fizz buzz"
-      case _ => number.toString
+      case (true, false) => "fizz"
+      case (false, true) => "buzz"
+      case (false, false) => number.toString
     }
 }
+
 
