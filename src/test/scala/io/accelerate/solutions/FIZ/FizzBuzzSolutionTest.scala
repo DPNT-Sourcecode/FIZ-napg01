5ef88ec670a32e46bd35a38dec31df782f0b92a7
sbx_user1051 <sbx_user1051@169.254.44.229>
2025-11-10 21:03:38 +0000

    Mon Nov 10 21:03:38 UTC 2025

---
+++ src/test/scala/io/accelerate/solutions/FIZ/FizzBuzzSolutionTest.scala
@@ -0,0 +1,34 @@
+package io.accelerate.solutions.FIZ
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class FizzBuzzSolutionTest extends AnyFlatSpec with Matchers {
+  "FizzBuzz" should {
+    "print 'fizz' for multiples of 3" in {
+      FizzBuzzSolution().fizzBuzz(3) shouldBe "fizz"
+      FizzBuzzSolution().fizzBuzz(9) shouldBe "fizz"
+      FizzBuzzSolution().fizzBuzz(99) shouldBe "fizz"
+    }
+
+  "print 'buzz' for multiples of 5" in {
+    FizzBuzzSolution().fizzBuzz(5) shouldBe "buzz"
+    FizzBuzzSolution().fizzBuzz(20) shouldBe "buzz"
+    FizzBuzzSolution().fizzBuzz(8000) shouldBe "buzz"
+  }
+
+ "print 'fizz buzz' for multiples of 3 and 5" in {
+    FizzBuzzSolution().fizzBuzz(15) shouldBe "fizz buzz"
+    FizzBuzzSolution().fizzBuzz(900) shouldBe "fizz buzz"
+    FizzBuzzSolution().fizzBuzz(9000) shouldBe "fizz buzz"
+  }
+
+  "print the number for anything else" in {
+    FizzBuzzSolution().fizzBuzz(0) shouldBe "0"
+    FizzBuzzSolution().fizzBuzz(1) shouldBe "1"
+    FizzBuzzSolution().fizzBuzz(8) shouldBe "8"
+    FizzBuzzSolution().fizzBuzz(101) shouldBe "101"
+    FizzBuzzSolution().fizzBuzz(9998) shouldBe "9998"
+  }
+  }
+}
